c2e2d29b5917f7a910b2633b9c0d48fe1ae8d91f
asusn <asusn.online@gmail.com>
2024-08-04 01:02:28 +0900

    花火のアニメーション追加

--- const.py
+++
@@ -1,3 +0,0 @@
-size = 10
-fps = 5
-frame_duration = 1 / fps 
--- hanabi.py
+++ hanabi.py
@@ -1,9 +1,10 @@
 import time
 import random
 import os
-import sys
-from const import *
 from math import cos, sin
+
+fps = 5
+frame_duration = 1 / fps
 
 class Sky:
     def __init__(self, height=20, width=40):
@@ -17,7 +18,7 @@ class Sky:
 
     def print_sky(self):
         for line in self.canvas:
-            print('\033[91m' + ''.join(line) + '\033[0m', file=sys.stderr)
+            print(''.join(line))
 
 class Firework:
     def __init__(self, sky, size=10):
@@ -31,18 +32,24 @@ class Firework:
         self.sky.canvas[current_height][self.center_x] = '|'
         self.sky.print_sky()
 
-    def print_burst(self):
+    def print_burst(self, current_frame, total_frames):
         self.sky.clear()
-        for _ in range(random.randint(5, 15)):
-            length = random.randint(2, self.size)
-            angle = random.random() * 360
-            rad = angle * (3.14159 / 180)
-            
-            for i in range(1, length):
-                x = int(self.center_x + i * cos(rad))
-                y = int(self.center_y + i * sin(rad))
+        is_fade = (current_frame == 0 or current_frame == total_frames - 1)
+        symbols = ['.', 'o', '+', '*'] if not is_fade else ['.']
+        max_radius = self.size
+        aspect_ratio = 2 # 真円に見えるように横方向にスケーリング
+
+        current_radius = min(max_radius, int(max_radius * ((current_frame + 1) / 3)))
+
+        for radius in range(1, current_radius + 1):
+            symbol = random.choice(symbols)
+            for angle_deg in range(0, 360, 15):
+                angle_rad = angle_deg * (3.14159 / 180)
+                x = int(self.center_x + aspect_ratio * radius * cos(angle_rad) + 0.5)
+                y = int(self.center_y + radius * sin(angle_rad) + 0.5)
                 if 0 <= x < self.sky.width and 0 <= y < self.sky.height:
-                    self.sky.canvas[y][x] = '*'
+                    self.sky.canvas[y][x] = symbol
+
         self.sky.print_sky()
     
     def wait(self, frames):
@@ -51,8 +58,7 @@ class Firework:
         time.sleep(frame_duration * frames)
     
     def launch(self, rising_frames, wait_frames, burst_frames):
-        step_heights = [int(self.sky.height - 1 - i * (self.sky.height // 2 / rising_frames)) for i in range(rising_frames)]
-
+        step_heights = [int(self.sky.height - 1 - i * (self.sky.height // 2 / (rising_frames + 1))) for i in range(rising_frames)]
         for height in step_heights:
             self.sky.clear()
             self.print_rising(height)
@@ -60,22 +66,24 @@ class Firework:
 
         self.wait(wait_frames)
         
-        for _ in range(burst_frames):
+        for current_frame in range(burst_frames):
             self.sky.clear()
-            self.print_burst()
+            
+            self.print_burst(current_frame, burst_frames)
             time.sleep(frame_duration)
 
 def main():
-    sky = Sky(height=20, width=40)
-    firework = Firework(sky=sky, size=10)
+    sky = Sky(height=30, width=60)
+    firework = Firework(sky=sky, size=8)
     try:
         firework.wait(2)
-        firework.launch(rising_frames=5, wait_frames=3, burst_frames=6)
+        firework.launch(rising_frames=9, wait_frames=5, burst_frames=9)
         firework.wait(2)
-        firework.launch(rising_frames=10, wait_frames=5, burst_frames=6)
+        firework.launch(rising_frames=8, wait_frames=4, burst_frames=8)
+        firework.wait(2)
     except KeyboardInterrupt:
         sky.clear()
-        print("\033[91m花火大会は終了しました\033[0m", file=sys.stderr)
+        print('花火大会は終了しました')
 
 if __name__ == '__main__':
     main()
